026e5cab81ad01217e9cdde8670b92eda3abf6c3
Sanjai <sanjai.r@zadroit.com>
2023-11-29 18:49:28 +0530

    Initial commit

--- src/main/java/com/example/timesheet/Respositories/EmployeeClientdetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.timesheet.Respositories;
-
-import com.example.timesheet.Entity.EmployeeClientdetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeClientdetailsRepository extends JpaRepository<EmployeeClientdetails,Integer> {
-}
