4d70254e88ed2cc9c8f012de0cef09b32527a03d
sanjai <150254508+sanjai1705@users.noreply.github.com>
2023-12-07 18:12:32 +0530

    Delete src directory

--- src/utils/sidebarRouteUtil.js
+++
@@ -1,9 +0,0 @@
-import sideBarRoute from '../data/sidebarRoute';
-
-export function getRoutesByRole(role) {
-  if (sideBarRoute.hasOwnProperty(role)) {
-    return sideBarRoute[role];
-  } else {
-    return {};
-  }
-}
